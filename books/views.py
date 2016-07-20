917f1d4aaeb72d2e38af81d88a3b0bfb20a48021
Yjing <18569095589@163.com>
2016-07-20 23:25:24 +0800

    add a origin view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from books.models import book
 
 # Create your views here.
+def home(request):
+    book_list = []
+    for id in range(1, 10):
+        book_list.append(book.objects.get(book_id = id))
+    return render(request,  'home.html', {'book_list': book_list, 'title': 'book_website',})
