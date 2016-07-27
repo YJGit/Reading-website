05470a365625e0e59815d48d0c700761099fd8cb
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-07-28 05:33:49 +0800

    try to handle scoring star with js.

---
+++ static/js/detail.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ma_Zi_jun on 2016/7/28.
+ */
+$(function () {
+  var fullMark = 5;
+  $star = $('.score-star');
+  $star.bind('mouseover', function() {
+      var order = $star.index($(this));
+      for (var j = 0; j < fullMark; j++){
+          if (j <= order){
+              $
+          }
+      }
+  });
+
+})
